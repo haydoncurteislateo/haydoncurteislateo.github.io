60578b43a7ed51eddb0e4c72c6757f7dc0241770
Haydon Curteis-Lateo <haydon1900@gmail.com>
2018-02-12 20:20:30 +0000

    V2 Update

--- custom/testing/V2/js/scripts.js
+++ custom/testing/V2/js/scripts.js
@@ -4,14 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
     randomInterval = Math.floor((Math.random()*8000)+5000);
     backgroundChange();
     h1BackgroundChange();
+    scrollH4Change();
+    scrollArrowChange();
     setTimeout(repeat, randomInterval);
-})();
+  });
+});
 
 
 // Functions Start
 // Landing Screen Start
 
-var  animationDuration = Math.floor((Math.random()*5000)+1000);
+var  animationDuration = Math.floor((Math.random()*5000)+2000);
 function h1BackgroundChange() {
   document.getElementById("landingScreenName").style["animation-duration"] = animationDuration + "ms";
   setTimeout(function(){ h1play() }, 700);
@@ -21,6 +24,16 @@ function backgroundChange() {
   document.getElementById("landingScreen").style["animation-duration"] = animationDuration + "ms";
   setTimeout(function(){ play() }, 1000);
   setTimeout(function(){ stop() }, animationDuration + 1000);
+}
+function scrollH4Change() {
+  document.getElementById("scroll").style["animation-duration"] = animationDuration + "ms";
+  setTimeout(function(){ playScrollH4() }, 700);
+  setTimeout(function(){ stopScrollH4() }, animationDuration + 1000);
+}
+function scrollArrowChange() {
+  document.getElementById("scrollArrow").style["animation-duration"] = animationDuration + "ms";
+  setTimeout(function(){ playScrollArrow() }, 700);
+  setTimeout(function(){ stopScrollArrow() }, animationDuration + 1000);
 }
 //Background color aniamtion state $Start$
 function play() {
@@ -45,10 +58,37 @@ function h1stop() {
   name.classList.remove("h1play");
   name.classList.add("h1stop");
 }
+// Scroll colours changing
+function playScrollH4() {
+  var name = document.getElementById("scroll");
+  name.classList.remove("scrollh4stop");
+  name.classList.add("scrollh4play");
+}
+function stopScrollH4() {
+  var name = document.getElementById("scroll");
+  name.classList.remove("scrollh4play");
+  name.classList.add("scrollh4stop");
+}
+function playScrollArrow() {
+  var name = document.getElementById("scrollArrow");
+  name.classList.remove("scrollarrowstop");
+  name.classList.add("scrollarrowplay");
+}
+function stopScrollArrow() {
+  var name = document.getElementById("scrollArrow");
+  name.classList.remove("scrollarrowplay");
+  name.classList.add("scrollarrowstop");
+}
 //Background H1 color aniamtion state $Finish$
 // Landing Screen Finish
-// Content Top Start
-// Content Top Finish
+
+// PreLoader Start
+  $(window).on('load', function(){
+    $("#loader").fadeOut();
+    $("#preLoader").delay(500).fadeOut();
+  })
+
+// PreLoader End
 
 // Parallax
 $( document ).ready(function() {
@@ -57,6 +97,10 @@ $( document ).ready(function() {
     var $scroll;
     var didScroll = false;
     var projectHeightValue = $("#projectExample1").height();
+    var landingScreenHeight = $("#landingScreen").outerHeight();
+    var landingScreenLocBottom = $("#landingScreen").offset().top + landingScreenHeight;
+    var portfolioLoc = landingScreenLocBottom-100;
+    var aboutLoc = $("#about").offset().top - 100;
     var i
 
     $(window).scroll(function(){
@@ -72,6 +116,18 @@ $( document ).ready(function() {
         if (scrollN >= skillsLoc) {
           skills();
         }
+        if (scroll >= portfolioLoc) {
+          showNav();
+        }
+        if (scroll <= portfolioLoc) {
+          hideNav();
+        }
+        if (scroll >= aboutLoc) {
+          changeNavColor();
+        }
+        if (scroll <= aboutLoc) {
+          resetNavColor();
+        }
         for (i = 1; i < 3; i++) {
           if ($scroll >= locationPos[i]) {
             elementsReveal();
@@ -79,12 +135,6 @@ $( document ).ready(function() {
         }
       }
     }, 500);
-
-
-
-    function landingScreen() {
-
-    }
 
 
     //Elements revealing
@@ -148,31 +198,12 @@ $( document ).ready(function() {
     }
 
 
-
+    var pauseScroll;
     var windowHeight = $(window).height();
     var scroll;
-    var aboutHeader = $('#aboutHeader');
-    var aboutHeaderLoc = $('#aboutHeader').offset().top;
-    var aboutHeaderHeight = $('#aboutHeader').height();
-    var aboutContainerLoc = $('#aboutContainer').offset().top;
-    var aboutHeaderLocCheck = true;
-    var pauseAboutHeader = true;
-
     $(window).scroll(function(){
         // Landing Screen Name
           scroll = $(window).scrollTop();
-          var name = $('#landingScreenName');
-          var nameLoc = $('#landingScreenName').offset().top;
-          var nameLocBottom = $('#landingScreenName').outerHeight();
-
-          if (scroll<=nameLoc) {
-            $('#landingScreenName').css({'transform' : 'translate(0px, '+scroll+'%)'});
-            $('#landingScreenName').css({'opacity' : ''+ 1-scroll/500+''});
-            var pauseScroll = scroll;
-          }
-          else {
-            $('#landingScreenName').css({'transform' : 'translate(0px, '+pauseScroll+'%)'});
-          }
         // Project Title
         var introPos = $('#portfolioIntro').offset().top;
         var introHeight = $('#portfolioIntro').outerHeight();
@@ -183,37 +214,7 @@ $( document ).ready(function() {
         else {
           $('#portfolioIntro').css({'top' : ''+pauseScroll+''});
         }
-
-        //About header
-        var scrollW = scroll+windowHeight;
-        var scrollPause = scrollW-(windowHeight/5);
-
-        if (pauseAboutHeader) {
-          if (scrollW >= aboutHeaderLoc) {
-            $('#aboutHeader').css({'transform': 'translate(0,'+(scroll-(aboutHeaderLoc-windowHeight))/3+'%)'});
-          }
-          if (scrollPause >= aboutContainerLoc) {
-            if (aboutHeaderLocCheck) {
-              aboutHeaderLocCheck = false;
-              pauseAboutHeader = false;
-              var aboutHeaderPausedLoc = (scroll-(aboutHeaderLoc-windowHeight))/3;
-            }
-          }
-        }
-        if (scrollPause <= aboutContainerLoc) {
-          aboutHeaderLocCheck = true;
-          pauseAboutHeader = true;
-        }
     });
-
-
-
-
-
-
-
-
-
 
     // Skills up and down on entering view
     var skillsLoc = $('#skills').offset().top;
@@ -262,20 +263,147 @@ $( document ).ready(function() {
 
   }
 
+  var changingNavColor = false;
+  var navOpen = false;
+
+    //Scroll Navigation
+    function showNav() {
+      $("#headerNavigation").css({'visibility': 'visible'});
+    }
+    function hideNav() {
+      $("#headerNavigation").css({'visibility': 'hidden'});
+    }
+    function changeNavColor() {
+      $("#showMenu").animate({'color':'white'}, 20);
+      $("#hideMenu").animate({'color':'black'}, 20);
+      $("#navigation a").animate({'color':'black'}, 100);
+      if (navOpen) {
+        $("#headerNavigation").animate({'background-color': 'white'},100);
+      }
+      else {
+        $("#headerNavigation").animate({'background-color': 'transparent'},100);
+      }
+      changingNavColor = true;
+    }
+    function resetNavColor() {
+      $("#showMenu").animate({'color':'black'}, 20);
+      $("#hideMenu").animate({'color':'white'}, 20);
+      $("#navigation a").animate({'color':'white'}, 100);
+      if (navOpen) {
+        $("#headerNavigation").animate({'background-color': 'black'},100);
+      }
+      else {
+        $("#headerNavigation").animate({'background-color': 'transparent'},100);
+      }
+      changingNavColor = false;
+    }
+
+    //Show Navigation
+    $("#showMenu").click(function(event) {
+      event.preventDefault();
+      $("#navigation").animate({"margin-left": "0"}, 300);
+      $("#showMenu").css({"display": "none"});
+      $("#hideMenu").css({"display": "block"});
+      if (changingNavColor) {
+        $("#headerNavigation").animate({"background-color": "white"}, 500);
+        $("#hideMenu").animate({"color": "black"}, 200);
+      }
+      else {
+        $("#headerNavigation").animate({"background-color": "rgb(0, 0, 0)"}, 500);
+        $("#hideMenu").animate({"color": "white"}, 200);
+      }
+      navOpen = true;
+    });
+    // Hide Navigation
+    $("#hideMenu").click(function(event) {
+      event.preventDefault();
+      $("#navigation").animate({"margin-left": "-100%"}, 300);
+      $("#hideMenu").css({"display": "none"});
+      $("#showMenu").css({"display": "block"});
+      if (changingNavColor) {
+        $("#showMenu").css({"color": "white"});
+      }
+      else {
+        $("#showMenu").css({"color": "black"});
+      }
+      navOpen = false;
+      $("#headerNavigation").animate({"background-color": "transparent"}, 500);
+    });
+
+    $(".scrollTo").click(function(event) {
+      $("#navigation").animate({"margin-left": "-100%"}, 300);
+      $("#headerNavigation").animate({"background-color": "transparent"}, 500);
+      $("#hideMenu").css({"display": "none"});
+      $("#showMenu").css({"display": "block"});
+      if (changingNavColor) {
+        $("#showMenu").css({"color": "white"});
+      }
+      else {
+        $("#showMenu").css({"color": "black"});
+      }
+      navOpen = false;
+    });
 
 
+    // Scroll Down
+    $("#scrollDown").click(function(event) {
+      event.preventDefault();
+      var scrollTarget = $("#scrollProjects").offset().top;
+      $('html, body').animate({scrollTop: scrollTarget}, 1000);
+    });
 
+    // Scroll to location
+    var scrollLink = $('.scrollTo');
+    scrollLink.click(function(e) {
+      e.preventDefault();
+      $('body,html').animate({scrollTop: $(this.hash).offset().top}, 1000);
+    });
 
+    //Change active Link
 
+    var scrollHome = $("#scrollHome").offset() - 100;
+    var scrollProjects = $("#scrollProjects").offset();
+    var scrollAbout = $("#scrollAbout").offset();
+    var scrollContact = $("#scrollContact").offset();
+    var scrollSkills = $("#scrollSkills").offset();
 
+    $(document).ready(function() {
+      $(window).scroll(function() {
 
+        var screenPosition = $(document).scrollTop() + 50;
 
+        if (screenPosition <= scrollHome.top) {
+          $( ".activeHome" ).removeClass( "active" );
+        }
+        if (screenPosition >= scrollHome.top) {
+          $( ".activeHome" ).addClass( "active" );
+          $( ".activeProjects" ).removeClass( "active" );
+        }
+        if (screenPosition >= scrollProjects.top) {
+          $( ".activeProjects" ).addClass( "active" );
+          $( ".activeHome" ).removeClass( "active" );
+          $( ".activeAbout" ).removeClass( "active" );
+        }
+        if (screenPosition >= scrollAbout.top) {
+          $( ".activeAbout" ).addClass( "active" );
+          $( ".activeProjects" ).removeClass( "active" );
+          $( ".activeContact" ).removeClass( "active" );
+        }
+        if (screenPosition >= scrollContact.top) {
+          $( ".activeContact" ).addClass( "active" );
+          $( ".activeAbout" ).removeClass( "active" );
+          $( ".activeSkills" ).removeClass( "active" );
+        }
+        // if (screenPosition <= scrollSkills.top) {
+        //   $( ".activeContact" ).removeClass( "active" );
+        // }
+        if (screenPosition >= scrollSkills.top) {
+          $( ".activeSkills" ).addClass( "active" );
+          $( ".activeContact" ).removeClass( "active" );
+        }
+      })
+    })
 
-
-
-
-
-    // Effect on image when hovered
 
     // Change browser title on leaving
     $(function() {
@@ -286,7 +414,8 @@ $( document ).ready(function() {
         if (original) {
           document.title = original;
         }
-      }).blur(function() {
+      })
+      .blur(function() {
         var title = $('title').text();
         if (title != message) {
           original = title;
@@ -294,7 +423,6 @@ $( document ).ready(function() {
         document.title = message;
       });
     });
-    }, false);
 
 });
 // Functions End
